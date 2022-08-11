4789299f88124f2fc3670a53914df562ac6f1c59
NealWills <nealwills93@gmail.com>
2022-08-11 20:38:47 +0800

    0.0.2 test

--- Example/MFFileDownloader/MFViewController.m
+++ Example/MFFileDownloader/MFViewController.m
@@ -25,41 +25,60 @@
     MFFileDownloaderLog.setMinLogLevel(MFFileDownloaderLogLevelNormal);
     [MFFileDownloaderFMDBManager defaultConfigure];
 
-    MFFileDownloaderFileModel *insertModel = [[MFFileDownloaderFileModel alloc] init];
-    insertModel.url = @"www.baidu.com2";
-    insertModel.mediaType = 6;
-    insertModel.name = @"test2.pag";
-    MFFileDownloaderCommonResultModel *insertResult = [MFFileDownloaderFMDBManager insertDataWithModel:insertModel];
-    if (insertResult.status >= 0) {
-        MFFileDownloaderLog.logDebug(@"数据添加成功");
+    MFFileDownloaderFileModel *fileModel = [[MFFileDownloaderFileModel alloc] init];
+    fileModel.url = @"www.baidu.com2";
+    fileModel.mediaType = 6;
+    fileModel.name = @"test2.pag";
+    MFFileDownloaderCommonResultModel *downloadResult = [MFFileDownloader addDownloadFile:fileModel];
+    if (downloadResult.status < 0) {
+        MFFileDownloaderLog.logError(downloadResult.msg);
     } else {
-        MFFileDownloaderLog.logDebug(insertResult.msg);
+        MFFileDownloaderLog.logDebug(@"下载开始");
     }
 
-    MFFileDownloaderCommonResultModel *searchModel = [MFFileDownloaderFMDBManager searchDataWithId:@"3"];
-
-    if ([searchModel.data isKindOfClass:[NSArray class]]) {
-        NSArray *list = searchModel.data;
-        if (list.count > 0) {
-            id obj = list.firstObject;
-            if ([obj isKindOfClass:MFFileDownloaderFileModel.class ]) {
-                MFFileDownloaderFileModel *fileModel = obj;
-                fileModel.downloadStatus = 1;
-                fileModel.version = 3;
-                fileModel.status = 2;
-                fileModel.furUrl = @"123123";
-                MFFileDownloaderCommonResultModel *editResult = [MFFileDownloaderFMDBManager updateDataWithModel:fileModel];
-                if (editResult.status >= 0) {
-                    MFFileDownloaderLog.logDebug(@"数据更新成功");
-                } else {
-                    MFFileDownloaderLog.logDebug(editResult.msg);
-                }
-            }
-        }
+    MFFileDownloaderCommonResultModel *reDownloadResult = [MFFileDownloader reDownloadFile:fileModel];
+    if (reDownloadResult.status < 0) {
+        MFFileDownloaderLog.logError(reDownloadResult.msg);
+    } else {
+        MFFileDownloaderLog.logDebug(@"下载开始");
     }
 
-    MFFileDownloaderCommonResultModel *search2Model = [MFFileDownloaderFMDBManager searchDataWithId:@"3"];
-    MFFileDownloaderLog.logDebug(search2Model.data);
+
+
+//    insertModel.url = @"www.baidu.com2";
+//    insertModel.mediaType = 6;
+//    insertModel.name = @"test2.pag";
+//    MFFileDownloaderCommonResultModel *insertResult = [MFFileDownloaderFMDBManager insertDataWithModel:insertModel];
+//    if (insertResult.status >= 0) {
+//        MFFileDownloaderLog.logDebug(@"数据添加成功");
+//    } else {
+//        MFFileDownloaderLog.logDebug(insertResult.msg);
+//    }
+//
+//    MFFileDownloaderCommonResultModel *searchModel = [MFFileDownloaderFMDBManager searchDataWithId:@"3"];
+//
+//    if ([searchModel.data isKindOfClass:[NSArray class]]) {
+//        NSArray *list = searchModel.data;
+//        if (list.count > 0) {
+//            id obj = list.firstObject;
+//            if ([obj isKindOfClass:MFFileDownloaderFileModel.class ]) {
+//                MFFileDownloaderFileModel *fileModel = obj;
+//                fileModel.downloadStatus = 1;
+//                fileModel.version = 3;
+//                fileModel.status = 2;
+//                fileModel.furUrl = @"123123";
+//                MFFileDownloaderCommonResultModel *editResult = [MFFileDownloaderFMDBManager updateDataWithModel:fileModel];
+//                if (editResult.status >= 0) {
+//                    MFFileDownloaderLog.logDebug(@"数据更新成功");
+//                } else {
+//                    MFFileDownloaderLog.logDebug(editResult.msg);
+//                }
+//            }
+//        }
+//    }
+//
+//    MFFileDownloaderCommonResultModel *search2Model = [MFFileDownloaderFMDBManager searchDataWithId:@"3"];
+//    MFFileDownloaderLog.logDebug(search2Model.data);
 
 }
 
